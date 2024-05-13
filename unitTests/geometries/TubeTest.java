e488a97d587b9612f8a62f4ebef8b0259c8acb0b
hadarNagar <hadarn26@gmail.com>
2024-05-13 14:06:13 +0300

    finish the tests for getNormal in cylinder and tube tests

--- unitTests/geometries/TubeTest.java
+++ unitTests/geometries/TubeTest.java
@@ -2,6 +2,7 @@ package geometries;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Ray;
+import primitives.Util;
 import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,10 +15,12 @@ class TubeTest {
         Tube t=new Tube(1,new Ray(new Point(1,1,1),new Vector(0,1,0)));
         Vector result=t.getNormal(new Point(1,3,2));
         assertEquals(1,result.length(),"ERROR: Triangle's normal is not a unit vector");
-        assertEquals(new Vector(0,0,1),result,"\"ERROR: Tube's getNormal(Point p) does not work correctly\"");
+        assertEquals(new Vector(0,0,1),result,"ERROR: Tube's getNormal(Point p) does not work correctly");
 
         // =============== Boundary Values Tests ==================
         // TC01: Test that
+        Vector result2=t.getNormal(new Point(1,1,2));
+        assertEquals(new Vector(0,0,1),result2,"ERROR: Tube's getNormal(Point p) does not work correctly");
 
     }
   
