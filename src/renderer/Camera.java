81c3699f19e1964c863b72e193bb64ab41fad7e9
hadarNagar <hadarn26@gmail.com>
2024-05-28 21:28:12 +0300

    create Camera class and implement her , create CameraTest and ItegrationCameraTest

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -2,7 +2,10 @@ package renderer;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Util;
 import primitives.Vector;
+
+import java.util.MissingResourceException;
 
 public class Camera implements Cloneable{
 
@@ -53,6 +56,17 @@ public class Camera implements Cloneable{
         return null;
     }
 
+    @Override
+    public Camera clone() {
+        try {
+            Camera clone = (Camera) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
     public static class Builder{
 
         final private Camera camera = new Camera();
@@ -66,19 +80,52 @@ public class Camera implements Cloneable{
 //        }
 
       public Builder setLocation(Point p0){
-
+          camera.position=p0;
+          return this;
       }
 
       public Builder setDirection(Vector vUp, Vector vTo){
-
+          if(!(Util.isZero(vUp.dotProduct(vTo))))
+              throw new IllegalArgumentException("The vectors aren't ortogonal");
+          camera.vUp=vUp.normalize();
+          camera.vTo=vTo.normalize();
+          return this;
       }
 
       public Builder setVpSize(Double height, Double width){
 
+          if(height<=0||width<=0)
+              throw new IllegalArgumentException("width and height should be positive");
+          camera.height=height;
+          camera.width=width;
+          return this;
       }
 
       public Builder setVpDistance(Double distance){
 
+          if(distance<=0)
+              throw new IllegalArgumentException("distance should be not negative");
+          camera.distance=distance;
+          return this;
+      }
+
+      public Camera build(){
+          if(camera.position==null)
+              throw new MissingResourceException("missing rendering data","Camera","camera's position");
+          if(camera.vTo==null)
+              throw new MissingResourceException("missing rendering data","Camera","camera's vTo");
+          if(camera.vUp==null)
+              throw new MissingResourceException("missing rendering data","Camera","camera's vUp");
+          if(Util.alignZero(camera.height)<=0)
+              throw new MissingResourceException("missing rendering data","Camera","camera's height");
+          if(Util.alignZero(camera.width)<=0)
+              throw new MissingResourceException("missing rendering data","Camera","camera's width");
+          if(Util.alignZero(camera.distance)<=0)
+              throw new MissingResourceException("missing rendering data","Camera","camera's distance");
+
+          camera.vRight=(camera.vTo.crossProduct(camera.vUp)).normalize();
+
+          return (Camera)camera.clone();
       }
 
     }
---
+++ unitTests/renderer/IntegrationCameraTest.java
@@ -0,0 +1,19 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+class IntegrationCameraTest {
+
+    @Test
+    void testSphere() {
+    }
+
+    @Test
+    void testTriangle() {
+    }
+
+    @Test
+    void testPlane() {
+    }
+
+}
